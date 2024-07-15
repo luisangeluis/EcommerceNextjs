cbead8d11a7150123cadb612a3cc10c86fa19f0c
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2024-07-15 21:43:29 +0000

    ui: Added option to dropdown of avatar

--- src/components/molecules/Avatar/Avatar.tsx
+++ src/components/molecules/Avatar/Avatar.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 //STYLES
 import styles from "./Avatar.module.scss";
-import DropDown from "../DropDown/DropDown";
 //COMPONENTS
+import DropDown from "../DropDown/DropDown";
+
+const list = [{ route: "my-purchases", displayName: "my purchases" }];
 
 const Avatar = ({ user, onClick, customClass }) => {
   const [openDropdown, setOpenDropdown] = useState(false);
@@ -14,7 +16,13 @@ const Avatar = ({ user, onClick, customClass }) => {
       <button className={styles.avatarBtn} onClick={handleClick}>
         {user?.firstName[0].toUpperCase()}
       </button>
-      {openDropdown && <DropDown userName={user?.firstName} />}
+      {openDropdown && (
+        <DropDown
+          userName={user?.firstName}
+          customClass={"btnThree"}
+          list={list}
+        />
+      )}
     </article>
   );
 };
--- src/components/molecules/DropDown/DropDown.tsx
+++ src/components/molecules/DropDown/DropDown.tsx
@@ -1,21 +1,27 @@
 import { useState } from "react";
 import styles from "./DropDown.module.scss";
 import BtnCustom from "@/components/atoms/BtnCustom/BtnCustom";
+import Link from "next/link";
 
-const DropDown = ({ customClass, userName }) => {
+const DropDown = ({ customClass, userName, list }) => {
   const handleClick = () => console.log("click");
 
   return (
     <article className={styles.DropDownContainer}>
       <div className={styles.DropDownBody}>
         <p>User:{userName}</p>
+        <BtnCustom onClick={handleClick} customClass={customClass}>
+          Log out
+        </BtnCustom>
         <ul className={styles.list}>
-          <li>
-            <BtnCustom onClick={handleClick} customClass={`${customClass}`}>
-              Log out
-            </BtnCustom>
-          </li>
-          <li>opcion 2</li>
+          {list.map((e, i) => {
+            return (
+              <li key={i}>
+                <Link href={`/${e.route}`}>{`${e.displayName}`}</Link>
+              </li>
+            );
+          })}
+          {/*<li>opcion 2</li>*/}
         </ul>
       </div>
     </article>
