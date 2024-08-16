0ebd21382ea28c95c5e3ca37dcc63aee5dc1a5c9
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2024-08-16 21:13:25 +0000

    Features: Added purchases component as an alternative to get the purchases with a token from the client

---
+++ src/components/organisms/PurchasesGroup/PurchasesGroup.tsx
@@ -0,0 +1,14 @@
+import styles from "./PurchasesGroup.module.scss";
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+
+const PurchaseGroup =()=>{
+
+  return(
+    <section className={styles.PurchasesGroup}>
+    </section>
+  )
+}
+
+expor defaul PurchaseGroup;
