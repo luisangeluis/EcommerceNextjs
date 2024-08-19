58585d99a3b8d071155dfaa542e0f35770284398
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2024-08-19 22:13:57 +0000

    Styles: Added styles to my-purchases view

--- src/app/my-purchases/page.tsx
+++ src/app/my-purchases/page.tsx
@@ -1,38 +1,54 @@
 "use client";
-
-import styles from "./myPurchases.module.scss";
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useRouter } from "next/navigation";
+//STYLES
+import styles from "./myPurchases.module.scss";
+//COMPONENTS
+import Title2 from "@/components/atoms/Title2/Title2";
+import CardPurchaseDetail from "@/components/molecules/CardPurchaseDetail/CardPurchaseDetail";
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 const MyPurchases = () => {
-  const userToken = useSelector((state) => state.userToken);
   const [purchases, setPurchases] = useState(null);
+  const router = useRouter();
 
   useEffect(() => {
     if (typeof window !== undefined) {
       const currentToken = localStorage.getItem("ecoUserToken");
 
-      getMyPurchases(currentToken)
-        .then((res) => res.json())
-        .then((res) => console.log(res))
-        .catch((err) => console.log(err));
+      if (currentToken) {
+        getPurchases(currentToken)
+          .then((res) =>res.json())
+          .then((res) => {
+            console.log(res.response);
+            setPurchases(res.response);
+          })
+          .catch((error) => console.log(error));
+      } else {
+        router.push("/");
+      }
     }
   }, []);
 
-  // const purchases = await getMyPurchases(userToken); 
-  return <section className={styles.miPurchases}></section>;
-};
+  const getPurchases = async (userToken: string) => {
+    const init = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+      },
+    };
 
-const getMyPurchases = async (userToken) => {
-  const init = {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${userToken}`,
-    },
+    const purchases = await fetch(`${apiUrl}/api/v1/orders/`, init);
+    return purchases;
   };
 
-  return await fetch(`${apiUrl}/api/v1/orders/`, init);
+  return (
+    <section className={styles.myPurchases}>
+      <Title2>My purchases</Title2>
+      {purchases?.map((e) => <CardPurchaseDetail purchaseDetail={e} key={e.id}/>)}
+    </section>
+  );
 };
+
 export default MyPurchases;
---
+++ src/components/molecules/CardPurchaseDetail/CardPurchaseDetail.tsx
@@ -0,0 +1,36 @@
+import SubTitle2 from "@/components/atoms/SubTitle2/SubTitle2";
+import styles from "./CardPurchaseDetail.module.scss";
+import SubTitle1 from "@/components/atoms/SubTitle1/SubTitle1";
+
+const CardPurchaseDetail = ({ purchaseDetail }) => {
+  console.log(purchaseDetail.createdAt);
+  console.log(new Date(purchaseDetail.createdAt));
+  const date = new Date(purchaseDetail.createdAt);
+
+  return (
+    <article className={styles.CardPurchaseDetailContainer}>
+      <div className={styles.header}>
+        <SubTitle2>Order number: {purchaseDetail.id}</SubTitle2>
+        <p>{`-- Created: ${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} --`}</p>
+        <br /> 
+      </div>
+      <div className={styles.body}>
+        {purchaseDetail.orderDetails.map((e) => {
+          return (
+            <div key={e.id}>
+            <div className={styles.detail}>
+              <p>Quantity: {e.quantity}</p>
+              <p>Price: {e.price}</p>
+              <SubTitle2>Subtotal: {e.subtotal}</SubTitle2>
+            </div>
+              <hr/>
+            </div>
+          );
+        })}
+      </div>
+      <div className={styles.footer}></div>
+    </article>
+  );
+};
+
+export default CardPurchaseDetail;
