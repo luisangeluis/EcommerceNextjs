ded5a253b43b60314749b670e3cf2f6bcaa1bbaf
luis gonzález <65631946+luisangeluis@users.noreply.github.com>
2024-08-30 21:09:32 +0000

    Feature: Fixing pagination, check ievery filter is aware of the others

--- src/components/organisms/PaginationMuiProducts/PaginationMuiProducts.tsx
+++ src/components/organisms/PaginationMuiProducts/PaginationMuiProducts.tsx
@@ -6,17 +6,21 @@ import { useDispatch, useSelector } from "react-redux";
 const PaginationMuiProducts = () => {
   const products = useSelector((state) => state.products);
   const termsToSearch = useSelector((state) => state.termsToSearch);
-  const [page, setPage] = useState(1);
   const dispatch = useDispatch();
+  //const [page, setPage] = useState(1);
 
-  useEffect(() => {
-    const currentPage = termsToSearch.page;
-    if (page !== currentPage) {
-      dispatch(setTermsToSearch({ ...termsToSearch, page }));
-    }
-  }, [page]);
+  // useEffect(() => {
+  //   const currentPage = termsToSearch.page;
+  //   if (page !== currentPage) {
+  //     dispatch(setTermsToSearch({ ...termsToSearch, page }));
+  //   }
+  // }, [page]);
 
-  const handleChange = (e, value) => setPage(value);
+  if (!products.totalPages) return;
+
+  const handleChange = (e, value) => {
+    dispatch(setTermsToSearch({ ...termsToSearch, page: value }));
+  };
 
   return (
     <PaginationMui
--- src/store/slices/termsToSearchSlice.ts
+++ src/store/slices/termsToSearchSlice.ts
@@ -2,15 +2,15 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const termsToSearchSlice = createSlice({
   name: "termsToSearch",
-    initialState: {
-    page: 1,
+  initialState: {
+    page: 0,
     productInfo: "",
     categoryId: "",
   },
-  reducers:{
-    setTermsToSearch:(state,action)=>action.payload
-  }
-})
+  reducers: {
+    setTermsToSearch: (state, action) => action.payload,
+  },
+});
 
 export default termsToSearchSlice.reducer;
-export const {setTermsToSearch} = termsToSearchSlice.actions;
+export const { setTermsToSearch } = termsToSearchSlice.actions;
