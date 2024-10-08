2a614cb4e1545f67f4a8e3b342a74dd48d1a7821
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2024-10-08 21:46:02 +0000

    Added announcement of user in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,19 @@
 import LoginForm from "@/components/molecules/LoginForm/LoginForm";
 import styles from "./login.module.scss";
+import SubTitle2 from "@/components/atoms/SubTitle2/SubTitle2";
 
 const login = () => {
   return (
     <section className={styles.loginContainer}>
+      <section className={`${styles.user} absolute`}>
+        <p>
+          User: <b>user.one@correo.com</b>
+        </p>
+        <p>
+          Password: <b>12345</b>
+        </p>
+      </section>
+
       <LoginForm />
     </section>
   );
